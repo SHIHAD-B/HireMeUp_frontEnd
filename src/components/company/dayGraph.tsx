0a0211a3cebb47ebe0eb98f8d0a3febef047d28d
SHIHAD-B <shihadb0@gmail.com>
2024-07-01 14:10:51 +0530

    feat:company dash dynamic data added

--- src/components/company/dayGraph.tsx
+++ src/components/company/dayGraph.tsx
@@ -1,16 +1,82 @@
-
+import { companyApplicantList } from '@/redux/actions/companyAction';
+import { fecthJob } from '@/redux/actions/jobAction';
+import { AppDispatch, RootState } from '@/redux/store';
 import { BarChart } from '@mui/x-charts/BarChart';
+import { useEffect, useState } from 'react'; 
+import { useDispatch, useSelector } from 'react-redux';
 
-export const ChartsOverviewDay=()=> {
+export const ChartsOverviewDay = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { data } = useSelector((state: RootState) => state.company);
+  const { data: jobs } = useSelector((state: RootState) => state.job);
+  const { data: applicants } = useSelector((state: RootState) => state.applicantList);
+
+  const [weekData, setWeekData] = useState<any[]>([]); 
+
+  useEffect(() => {
+    const update = async () => {
+      await dispatch(fecthJob(data?._id));
+      await dispatch(companyApplicantList(String(data?._id)));
+    };
+    update();
+  }, []);
+
+  useEffect(() => {
+   
+    const filterDataByWeek = () => {
+      const currentDate = new Date();
+      const currentDay = currentDate.getDay(); 
+      const startDate = new Date(currentDate); 
+      startDate.setDate(startDate.getDate() - currentDay); 
+      startDate.setHours(0, 0, 0, 0);
+      const endDate = new Date(startDate); 
+      endDate.setDate(endDate.getDate() + 6); 
+      endDate.setHours(23, 59, 59, 999);
+
+    
+      const filteredJobs = jobs?.filter((job: any) => {
+        const jobDate = new Date(job.createdAt); 
+        return jobDate >= startDate && jobDate <= endDate;
+      });
+
+      const filteredApplicants = applicants?.filter((applicant: any) => {
+        const applicantDate = new Date(applicant.createdAt); 
+        return applicantDate >= startDate && applicantDate <= endDate;
+      });
+
+  
+      const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+      const dayData: any[] = daysOfWeek.map(() => ({ jobCount: 0, applicantCount: 0 }));
+
+      
+      filteredJobs?.forEach((job: any) => {
+        const jobDay = new Date(job.createdAt).getDay(); 
+        dayData[jobDay].jobCount++;
+      });
+
+      filteredApplicants?.forEach((applicant: any) => {
+        const applicantDay = new Date(applicant.createdAt).getDay(); 
+        dayData[applicantDay].applicantCount++;
+      });
+
+     
+      const formattedData = daysOfWeek.map((_, index) => ({
+        data: [dayData[index].jobCount, dayData[index].applicantCount],
+        color: index % 2 === 0 ? 'violet' : 'cyan', 
+      }));
+
+      setWeekData(formattedData);
+    };
+
+    filterDataByWeek(); 
+  }, [jobs, applicants]);
+
   return (
     <BarChart
-      series={[
-        { data: [35, 44, 24, 34,51, 6, 49,],color:'violet' },
-        { data: [51, 6, 49, 30,20,10,20],color:"cyan" },
-      ]}
+      series={weekData} 
       height={290}
-      xAxis={[{ data: ['Mon', 'Tue', 'Wed', 'Thu','Fri','Sat','Sun'], scaleType: 'band' }]}
+      xAxis={[{ data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'], scaleType: 'band' }]}
       margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
     />
   );
-}
+};
